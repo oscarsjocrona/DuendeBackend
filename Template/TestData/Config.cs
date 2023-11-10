174ca810b92fcd8c98ab53065634523995b8b205
Oscar Sjöcrona <Oscar.Sjocrona@logevo.se>
2023-11-10 16:19:37 +0100

    fix

--- Template/TestData/Config.cs
+++ Template/TestData/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace ids.TestData
 {
@@ -9,6 +10,7 @@ namespace ids.TestData
             {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
+            new IdentityResources.Email(),
             new IdentityResource
             {
                 Name = "role",
@@ -37,32 +39,51 @@ namespace ids.TestData
             new Client[]
             {
             // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client", //Machine to machine
-                ClientName = "Client Credentials Client",
+                new Client
+                {
+                    ClientId = "m2m.client", //Machine to machine
+                    ClientName = "Client Credentials Client",
 
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                AllowedScopes = { "messagesapi.read", "messagesapi.write" }
-            },
+                    AllowedScopes = { "messagesapi.read", "messagesapi.write" }
+                },
 
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
-                AllowedGrantTypes = GrantTypes.Code,
+                    AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "http://localhost:5173/signin-oidc" },
-                FrontChannelLogoutUri = "http://localhost:5173/signout-oidc",
-                PostLogoutRedirectUris = { "http://localhost:5173/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:5444/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5444/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5444/signout-callback-oidc" },
+                    //RequireClientSecret = false,
+                    //RequirePkce =true,
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "messagesapi.read" },
+                    RequireConsent = true            
+                },
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientSecrets = { new Secret("b03651e6-ab90-4f8e-ad65-4e5d6f6f2dd1".Sha256()) },
 
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "messagesapi.read" },
-                RequireConsent = true            },
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "http://localhost:5173/signin-oidc" },
+                    FrontChannelLogoutUri = "http://localhost:5173/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:5173/signout-callback-oidc" },
+                    RequireClientSecret = false,
+                    RequirePkce =true,
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "messagesapi.read", IdentityServerConstants.StandardScopes.Email },
+                    RequireConsent = true,
+                    AllowRememberConsent = false,
+                }
             };
     }
 
